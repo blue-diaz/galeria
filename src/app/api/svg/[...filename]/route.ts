b364891c53d8dff4594f7984cdf74fc9f8c9ae9c
blue-diaz <blue-diaz@users.noreply.github.com>
2026-05-26 18:15:09 -0300

    ooo vida viu

--- src/app/api/svg/[...filename]/route.ts
+++ src/app/api/svg/[...filename]/route.ts
@@ -4,11 +4,7 @@ import { isValidDimension, manipulateSvgDimensions } from '../svgManipulator';
 const svgContentCache = new Map<string, string>();
 
 const configuredBaseOrigin =
-<<<<<<< HEAD
-  process.env.NEXT_PUBLIC_APP_URL ?? process.env.APP_URL;
-=======
   process.env['NEXT_PUBLIC_APP_URL'] ?? process.env['APP_URL'];
->>>>>>> dev
 
 if (!configuredBaseOrigin) {
   throw new Error(
